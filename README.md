3ca3eb7a4f6e4472f5f79e4bc6ed397a9776d414
Qurat238 <q5500407@gmail.com>
2023-07-16 17:29:23 +0500

    Git initialized for Ecommerce Store

---
+++ README.md
@@ -0,0 +1,27 @@
+## Install Dependencies
+**For Backend** - npm i
+**For Frontend** - cd frontend   
+                   npm i
+
+## Env Variables
+Create a config.env file in backend/config directory and add following variables:
+PORT=
+DB_URI=
+STRIPE_API_KEY=
+STRIPE_SECRET_KEY=
+JWT_SECRET=
+JWT_EXPIRE=
+COOKIE_EXPIRE=
+SMPT_SERVICE=
+SMPT_MAIL=
+SMPT_PASSWORD=
+SMPT_HOST=
+SMPT_PORT=
+CLOUDINARY_NAME=
+CLOUDINARY_API_KEY=
+CLOUDINARY_API_SECRET=
+Note: Fill each field with your information respectively.
+
+## About Me
+**Upwork** Click [Here](https://www.upwork.com/freelancers/~01a84faf07c15d6b11) **q5500407@gmail.com**
+**LinkedIn** Click [Here](https://www.linkedin.com/in/qurat-ul-ain-228609213/)  **sajidquratulain6@gmail.com**
---
+++ app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import errorMiddleware from "./backend/middleware/error.js";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, join, resolve } from 'path';
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload());
+
+
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    // config
+    dotenv.config({path:"backend/config/config.env"});
+}
+
+// Route Imports
+import product from "./backend/routes/productRoute.js";
+import user from "./backend/routes/userRoute.js";
+import order from "./backend/routes/orderRoute.js";
+import payment from "./backend/routes/paymentRoute.js";
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+//To run frontend and backend on same port
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(join(__dirname, "./frontend/build")));
+app.get("*",(req,res) => {
+    res.sendFile(resolve(__dirname, "./frontend/build/index.html"));
+});
+
+//Middleware for errors
+app.use(errorMiddleware);
+
+
+export default app;
